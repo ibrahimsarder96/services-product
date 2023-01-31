4a473d3c11bc7cc8fc31094ab5c47459051ebdac
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-02-01 01:31:20 +0600

    login page form add

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div>
-      <h1>this login</h1>
-    </div>
+  <div className='h-screen flex justify-center items-center'>
+      <h1 className='text-5xl font-bold text-pink-400'>Login</h1>
+  </div>
   );
 };
 
